ec9ec37fdeb4a25e97e04e48183bb973da2358a0
spmno <sunqingpeng@hotmail.com>
2015-02-26 22:23:29 +0800

    add bike view controller

---
+++ KuQi/BikeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BikeViewController.h
+//  KuQi
+//
+//  Created by ZhouM on 15/2/25.
+//  Copyright (c) 2015年 ZhouM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BikeViewController : UITableViewController
+
+@end
